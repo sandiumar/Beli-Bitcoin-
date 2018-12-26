70df80173611636e476ac8f3e08656b5ea6dda50
sandiumar <umaryuda123@gmail.com>
2018-12-26 11:04:44 +0700

    Menambahkan fitur disable button

--- app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
+++ app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
@@ -6,27 +6,24 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
    private EditText rp, btc;
+   Button lanjut;
    Double hsl;
-
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inisialisasi Variabel
         btc =  findViewById(R.id.nom_bitcoin);
-
         rp = findViewById(R.id.nom_rp);
+        lanjut = findViewById(R.id.btn_lanjut);
 
 
 
@@ -38,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String Btc = btc.getText().toString();
-                
-                //Error handling konversi bilangan
+                String Btc = btc.getText().toString().trim();
+
+                //Error handling btc
                 int BTC = 0;
                 try {
                     BTC = Integer.parseInt(Btc);
@@ -48,11 +45,20 @@ public class MainActivity extends AppCompatActivity {
                     nfe.printStackTrace();
                 }
 
-               int RP = Integer.parseInt("93650");
+                String Rp = "93650";
+                //Error handling rupiah
+                int RP = 0;
+                try {
+                     RP = Integer.parseInt(Rp);
+                } catch (NumberFormatException nfc){
+                    nfc.printStackTrace();
+                }
 
                 int hsl = BTC * RP;
                 rp.setText(String.valueOf(hsl));
 
+                //Disable Button Lanjut
+                lanjut.setEnabled(!Btc.isEmpty() && !Rp.isEmpty());
             }
 
             @Override
