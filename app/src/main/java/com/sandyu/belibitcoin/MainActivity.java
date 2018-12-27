a129b03591e15c54154679ef033de293ed268ec6
sandiumar <umaryuda123@gmail.com>
2018-12-27 08:36:47 +0700

    Konversi bug fixing

--- app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
+++ app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
@@ -9,11 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
    private EditText rp, btc;
    Button lanjut;
    Double hsl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,35 +41,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 String Btc = btc.getText().toString().trim();
+                String rP = rp.getText().toString().trim();
 
                 //Error handling btc
-                int BTC = 0;
+                long BTC = 0;
+
                 try {
-                    BTC = Integer.parseInt(Btc);
+                    BTC = Long.parseLong(Btc);
                 } catch (NumberFormatException nfe){
                     nfe.printStackTrace();
                 }
 
-                String Rp = "93650";
-                //Error handling rupiah
-                int RP = 0;
-                try {
-                     RP = Integer.parseInt(Rp);
+                //Format digit 3 angka
+                /*DecimalFormat formatter = new DecimalFormat("#,###,###");
+                String  rp_format = formatter.format(93650);
+                long hsl_konversi = Long.parseLong(rp_format); */
+
+
+                //Error handling rp
+                long RP = 93650;
+                try{
+                    RP = Long.parseLong(rP);
                 } catch (NumberFormatException nfc){
                     nfc.printStackTrace();
                 }
 
-                int hsl = BTC * RP;
+
+
+               Long hsl = BTC * RP;
                 rp.setText(String.valueOf(hsl));
 
                 //Disable Button Lanjut
-                lanjut.setEnabled(!Btc.isEmpty() && !Rp.isEmpty());
+                lanjut.setEnabled(!Btc.isEmpty() && !rP.isEmpty());
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
 
             }
+
+
         });
     }
 
