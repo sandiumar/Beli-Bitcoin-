ed370ef38f8c38379bb976fea298352373c51331
sandiumar <umaryuda123@gmail.com>
2018-12-31 21:53:23 +0700

    Add swipe refresh and random function

--- app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
+++ app/src/main/java/com/sandyu/belibitcoin/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sandyu.belibitcoin;
 
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,16 +10,26 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputLayout inputBtc;
+    private TextInputLayout inputRp;
+    private EditText rp, btc;
+    private SwipeRefreshLayout angka_random;
+    private TextView warning;
 
-   private EditText rp, btc;
+    //Set Variabel Widget
    Button lanjut;
    Double hsl;
+   double BTC = 0;
+   double RP = 93650;
+
 
 
     @Override
@@ -29,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
         btc =  findViewById(R.id.nom_bitcoin);
         rp = findViewById(R.id.nom_rp);
         lanjut = findViewById(R.id.btn_lanjut);
+        inputBtc = findViewById(R.id.input_bitcoin);
+        inputRp = findViewById(R.id.input_rp);
+        warning = findViewById(R.id.warn);
+        angka_random = findViewById(R.id.swipe_random);
+
+        //Untuk merubah angka btc dan rupiah secara random
+        angka_random.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                generateRandom();
+
+            }
+        });
 
 
 
@@ -44,20 +69,14 @@ public class MainActivity extends AppCompatActivity {
                 String rP = rp.getText().toString().trim();
 
                 //Error handling btc
-               double BTC = 0;
-               try {
+                try {
                    BTC = Double.parseDouble(Btc);
                }catch (NumberFormatException nfe){
                    nfe.printStackTrace();
                }
 
 
-
-
-
                 //Error handling rp
-                double RP = 93650;
-
                 try{
                     RP = Double.parseDouble(rP);
                 } catch (NumberFormatException nfc){
@@ -71,17 +90,33 @@ public class MainActivity extends AppCompatActivity {
                 rp.setText(String.valueOf(hsl_format));
 
                 //Disable Button Lanjut
-                lanjut.setEnabled(!Btc.isEmpty() && !rP.isEmpty());
+//                lanjut.setEnabled(!Btc.isEmpty() && !rP.isEmpty());
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
 
             }
-
-
         });
     }
+
+
+
+    private void generateRandom(){
+        double start = 1;
+        double end = 10;
+        double random = new Random().nextDouble();
+
+        //Menset hasil result agar memiliki format
+        NumberFormat result_format = new DecimalFormat("#,###,###");
+        double result = random * RP * Double.valueOf(btc.getText().toString());
+        String rst_format = result_format.format(result);
+
+        rp.setText(String.valueOf(result));
+        angka_random.setRefreshing(false);
+    }
+
+
 
 
 
